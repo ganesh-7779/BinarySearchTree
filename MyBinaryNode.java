6bea43a73e2213681a5d44b0e2fff661e314d335
Ganesh <ganeshgavhad9@gmail.com>
2021-06-14 23:09:08 +0530

    Added : UC2 added binart tree till 13 size

--- MyBinaryNode.java
+++ MyBinaryNode.java
@@ -1,10 +1,18 @@
 package com.bridgelabz.binary;
 
+/*************************************************
+ * Purpose: Implementation of Binary search tree
+ * @author Ganesh Gavhad
+ * @varsion 1.0
+ *************************************************/
 public class MyBinaryNode<K extends Comparable<K>> {
 	K key;
 	MyBinaryNode<K> left;
 	MyBinaryNode<K> right;
 
+/**
+ *Constructor for node variable
+ */
 public 	MyBinaryNode(K key) {
 	this.key=key;
 	this.left=null;
--- Runner.java
+++ Runner.java
@@ -1,8 +1,9 @@
 package com.bridgelabz.binary;
-
-/**
- * Runner for main method
- */
+/************************************************
+ *Purpose: Implementation of Binary search tree
+ * @author Ganesh Gavhad
+ * @version 1.0
+ *************************************************/
 public class Runner {
 
 	public static void main (String[]args) {
@@ -10,6 +11,16 @@ public class Runner {
 		element.add(56);
 		element.add(30);
 		element.add(70);
+		element.add(22);
+		element.add(40);
+		element.add(60);
+		element.add(95);
+		element.add(11);
+		element.add(65);
+		element.add(3);
+		element.add(16);
+		element.add(63);
+		element.add(67);
 		int size = element.getSize();
 		System.out.println(size);
 
